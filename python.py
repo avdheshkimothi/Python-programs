1cf88272504a596cd70acbf044d272f7fb7604e8
avdhesh kimothi <257133074+avdheshkimothi@users.noreply.github.com>
2026-08-04 09:30:46 +0530

    Delete python.py

--- python.py
+++
@@ -1,5 +0,0 @@
-import qrcode
-i=input("enter the url or text  you want to convert into qr code :")
-QR=qrcode.make(i)
-QR.save("your qr.png")
-print("qr has been successfully created and saved in your current directory as 'your qr.png'")
